22b98021ed3d50c065a461e7fb5f05a173584c80
Frankenfield <Kozyrov>
2017-06-22 16:09:48 -0700

    added a photo model to the database modeling. Also added new database field, called photo.

--- server/models/menu.js
+++ server/models/menu.js
@@ -12,6 +12,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false
         },
+        photo: {
+            type: DataTypes.VARCHAR,
+            allowNull: false
+        },
         description: {
             type: DataTypes.STRING,
             allowNull: false
