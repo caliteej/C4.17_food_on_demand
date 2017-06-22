72a9b1ecb3b0dda2fe1d4913ec91def6e2dee8fa
Jonathan Limtiaco <Jonathanlimtiaco@gmail.com>
2017-06-22 16:58:00 -0700

    Everything working from live data

--- food_results/food_results.js
+++ food_results/food_results.js
@@ -4,34 +4,21 @@ $(document).ready(function(){
         menuModal(this)
     });
 });
-    function getFoodData() {
-    $.ajax({
-        method: 'get',
-        dataType: 'json',
-        url: "../dummy_data/chefData_paul.json",
-        success: function displayFood(response) {
-            for (var i = 0; i < response.menu.data.length; i++) {
-                available_meals = response.menu.data;
-
-                var dish_card = $('<div>').addClass('food_item');
-                var dish_caption = $('<div>').addClass('food_caption');
-                var dish_photo = $('<img>').attr("src", response.menu.data[i].photo);
-                var dish_name = $('<h4>').addClass('food_name').text(response.menu.data[i].item_name);
-                var cost = $('<div>').addClass('food_cost').text(response.menu.data[i].price);
-
-                dish_caption.append(dish_name, cost);
-                dish_card.append(dish_photo, dish_caption);
-                dish_card.attr('id', response.menu.data[i].id);
-                $('#food_listing').append(dish_card);
-            }
-        },
-
-        error: function (response) {
-            console.log(response);
-        }
-    })
-};
 
 
+function displayFood() {
+    for (var i = 0; i < chefs[0].menu.data.length; i++) {
+        available_meals = chefs[0].menu.data;
 
+        var dish_card = $('<div>').addClass('food_item');
+        var dish_caption = $('<div>').addClass('food_caption');
+        var dish_photo = $('<img>').attr("src", chefs[0].menu.data[i].photo);
+        var dish_name = $('<h4>').addClass('food_name').text(chefs[0].menu.data[i].item_name);
+        var cost = $('<div>').addClass('food_cost').text(chefs[0].menu.data[i].price);
 
+        dish_caption.append(dish_name, cost);
+        dish_card.append(dish_photo, dish_caption);
+        dish_card.attr('id', chefs[0].menu.data[i].id);
+        $('#food_listing').append(dish_card);
+    }
+}
