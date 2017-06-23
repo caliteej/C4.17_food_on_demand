dbe56195421077d58972cfa382d303752f421d65
Jonathan Limtiaco <Jonathanlimtiaco@gmail.com>
2017-06-23 10:38:45 -0700

    email function integrated

--- checkout_mvp/checkout_script.js
+++ checkout_mvp/checkout_script.js
@@ -1,28 +1,9 @@
 var userName = '';
 var userEmail = '';
 
-
-$(document).ready(function displayMealInfo() {
-        console.log('function works');
-
-        $.ajax({
-            url: "../dummy_data/chefData_paul.json",
-            method: 'GET',
-            dataType: "JSON",
-            success: function (response) {
-                console.log(response);
-                var meal_name = $('<div>').text('Meal name: ' + response.menu.data[0].item_name);
-                var meal_photo = $('<img>').attr("src", response.menu.data[0].photo);
-                var meal_price = $('<div>').text('Meal price: ' + response.menu.data[0].price);
-                $('#orderOf_container').append(meal_name, meal_photo, meal_price);
-            }
-        });
-    }
-);
-
 //Confirmation modal and calls sendEmail function
 $(document).ready(function() {
-    $('button').click(function (){
+    $('#confirmButton').click(function (){
         userName = $("#userName").val();
         userEmail = $("#userEmail").val();
         console.log('user name is: ', userName);
@@ -56,3 +37,21 @@ function sendEmailConfirmation(userName, userEmail) {
 //     checkoutConfirm(final_meal);
 //         console.log(final_meal)
 // }
+
+// $(document).ready(function displayMealInfo() {
+//         console.log('function works');
+//
+//         $.ajax({
+//             url: "../dummy_data/chefData_paul.json",
+//             method: 'GET',
+//             dataType: "JSON",
+//             success: function (response) {
+//                 console.log(response);
+//                 var meal_name = $('<div>').text('Meal name: ' + response.menu.data[0].item_name);
+//                 var meal_photo = $('<img>').attr("src", response.menu.data[0].photo);
+//                 var meal_price = $('<div>').text('Meal price: ' + response.menu.data[0].price);
+//                 $('#orderOf_container').append(meal_name, meal_photo, meal_price);
+//             }
+//         });
+//     }
+// );
