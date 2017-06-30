8e8f773da10b14decf616810d96671611a15a8ce
Paul Villanueva <pmvillanueva@gmail.com>
2017-06-29 17:51:28 -0700

    style, buttons, content

--- public/static/js/menu_modal.js
+++ public/static/js/menu_modal.js
@@ -1,6 +1,7 @@
 var current_meal = null;
 var current_chef = null;
 var available_meals = [];
+
 function menuModal(meal){
     var img_id = meal.id;
     for(var i = 0; i < available_meals.length; i++){
@@ -13,38 +14,43 @@ function menuModal(meal){
         }
     }
     $('.modal-title').text(current_meal.item_name);
+
     var meal_div = $('<div>',{
         class: 'meal-modal col-xs-10'
     });
+
     var meal_img = $('<img>',{
         src: current_meal.photo,
         height: '175px',
         width: '275px',
         class: 'col-xs-8'
     });
+
     var meal_description = $('<h4>',{
         text: 'Description'
     });
+
     var meal_description_info = $('<p>',{
         text: current_meal.description
     });
+
     var meal_cost = $('<h4>',{
         text: 'Cost'
     });
+
     var meal_cost_info = $('<p>',{
         text: '$ ' + current_meal.price
     });
+
     $(meal_div).append(meal_img, meal_description, meal_description_info, meal_cost, meal_cost_info);
     $('.menu-modal').empty().append(meal_div);
     $('#single_menu_item_modal').modal('show');
 }
+
 function placeOrder(){
     console.log('Order Placed', current_meal);
     $('#landingPage').hide();
     $('#checkout').show();
-    var itemsOrdered = $('<h3>', {
-        text: 'Item(s):'
-    });
 
     var mealName = $('<h4>',{
         text: current_meal.item_name
@@ -66,7 +72,7 @@ function placeOrder(){
     });
 
     var chefNameLabel = $('<h5>',{
-        text: "Your nxtDoorChef:",
+        text: "Your nxtDoorChef:"
     });
 
     var chefName = $('<p>',{
@@ -83,7 +89,7 @@ function placeOrder(){
         target: "_blank"
     });
 
-    $('#orderOf_container').append(itemsOrdered, mealName, mealPhoto, mealDescription, mealCost);
+    $('#orderOf_container').append(mealName, mealPhoto, mealDescription, mealCost);
     $('#pickupInfo_container').append(chefNameLabel, chefName, chefAddressLabel, chefAddress);
     $('.backToHome').hide();
 }
